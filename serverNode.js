1d9e794b64db43e4456ca49c009cc1a4d84afc98
Juan1Diazr <juanestebandiaz018@gmail.com>
2024-05-10 10:07:01 -0500

    new

---
+++ serverNode.js
@@ -0,0 +1,32 @@
+var mysql = require("mysql2");
+var axios = require("axios");
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+app.use(cors());
+
+app.get("/data", function (req, res) {
+  var conexion = mysql.createConnection({
+    host: "localhost",
+    database: "usuarios",
+    user: "root",
+    password: "",
+  });
+
+  conexion.connect(function (err) {
+    if (err) throw err;
+    conexion.query("SELECT * FROM Usuario", function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+      conexion.end();
+    });
+  });
+});
+
+
+var puerto = 3001;
+
+app.listen(puerto, function () {
+  console.log(`App esta escuchando correctamente en el puesto ${puerto}`);
+});
